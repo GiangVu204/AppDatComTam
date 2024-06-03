34498553b2283a6f2abff9c7093c91905543388c
HaiDang04 <dangbhhph33065@fpt.edu.vn>
2024-06-03 09:55:18 +0700

    file mau va home cua HaiDang04

---
+++ app/src/main/java/com/example/appdatcomtam/Screen/ScreenAdim/ManagerScreen.kt
@@ -0,0 +1,15 @@
+package com.example.appdatcomtam.Screen.ScreenAdim
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ManagerScreen(navController: NavController? = null) {
+    Text(
+        text = "Chào mừng đến với quan ly san Pham ",
+
+        )
+}
+
+
