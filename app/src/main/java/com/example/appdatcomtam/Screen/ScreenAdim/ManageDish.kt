564a0f2e1c945f92d6c9c5295cc2c867d0d9a7b8
PC <vuhoanggiang2k4@gmail.com>
2024-06-05 09:58:51 +0700

    ManagerDish

--- app/src/main/java/com/example/appdatcomtam/Screen/ScreenAdim/ManageDish.kt
+++ app/src/main/java/com/example/appdatcomtam/Screen/ScreenAdim/ManageDish.kt
@@ -1,15 +1,25 @@
 package com.example.appdatcomtam.Screen.ScreenAdim
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -18,10 +28,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.toColorInt
@@ -30,8 +42,10 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.appdatcomtam.AppNavHost
 import com.example.appdatcomtam.R
+import com.example.appdatcomtam.ROUTE_NAME
 import com.example.appdatcomtam.Screen.MyBottomAppBar
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ManageDishScreen(navController: NavController? = null) {
@@ -47,7 +61,7 @@ fun ManageDishScreen(navController: NavController? = null) {
                 ),
                 title = {
                     Text(
-                        "Cum Tứm Dim ",
+                        "Cum tứm đim ",
                         color = Color.White,
                         fontSize = 17.sp,
                         fontWeight = FontWeight.Bold
@@ -56,19 +70,111 @@ fun ManageDishScreen(navController: NavController? = null) {
                 navigationIcon = {
                     Image(
                         painter = painterResource(id = R.drawable.logo_splash),
-                        contentDescription = "kkk",
+                        contentDescription = "logo",
                         modifier = Modifier
                             .size(70.dp)
                             .padding(10.dp)
                     )
                 },
 
-                )
+            )
         },
+        content = { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .background(color = Color.Black)
+            )
+            ManageDish(navController)
+        }
+    )
+}
+
+
+@Composable
+fun ManageDish(navController: NavController? = null) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 95.dp)
+            .background(color = Color("#252121".toColorInt()))
     ) {
-        it
+        // Quản lý món ăn
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+//                .clickable { navController?.navigate("${ROUTE_NAME.managerDish.name}") }
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_splash),
+                contentDescription = "Food Manager",
+                modifier = Modifier
+                    .size(55.dp)
+                    .align(Alignment.CenterStart)
+            )
+            Text(
+                text = "Thêm món ăn",
+                style = MaterialTheme.typography.titleLarge.copy(
+                    color = Color.White
+                ),
+                modifier = Modifier
+                    .padding(start = 80.dp, top = 10.dp)
+
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Quản lý loại món ăn
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_splash),
+                contentDescription = "Food Type Manager",
+                modifier = Modifier
+                    .size(55.dp)
+                    .align(Alignment.CenterStart)
+            )
+            Text(
+                text = "Sửa món ăn",
+                style = MaterialTheme.typography.titleLarge.copy(
+                    color = Color.White
+                ),
+                modifier = Modifier.padding(start = 80.dp, top = 10.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Quản lý loại món ăn
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_splash),
+                contentDescription = "Food Type Manager",
+                modifier = Modifier
+                    .size(55.dp)
+                    .align(Alignment.CenterStart)
+            )
+            Text(
+                text = "Xóa món ăn",
+                style = MaterialTheme.typography.titleLarge.copy(
+                    color = Color.White
+                ),
+                modifier = Modifier.padding(start = 80.dp, top = 10.dp)
+            )
+        }
     }
 }
+
 
 //@Composable
 //fun TopBar () {
